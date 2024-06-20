97a3508b7c4bce4f87a1955701c89d68a6bc2866
Ingrid614 <111050203+Ingrid614@users.noreply.github.com>
2024-06-20 14:55:00 +0100

    Update movie.dart

--- lib/data/models/movie.dart
+++ lib/data/models/movie.dart
@@ -12,45 +12,30 @@ class Movie {
   int32? note;
   DateTime? dateDeSortie
 
-  Command({
+  Movie({
     required this.id,
     required this.createdAt,
     required this.updatedAt,
-    required this.userId,
-    required this.clientId,
-    required this.adresseKin,
-    required this.montant,
-    required this.taux,
-    required this.cout,
-    required this.statutId,
-    required this.client,
+    required this.synopsis,
+    required this.note,
+    required this.dateDeSortie
   });
 
-  factory Command.fromJson(Map<String, dynamic> json) => Command(
+  factory Movie.fromJson(Map<String, dynamic> json) => Movie(
     id: json["id"],
     createdAt: DateTime.parse(json["created_at"]),
     updatedAt: DateTime.parse(json["updated_at"]),
-    userId: json["user_id"],
-    clientId: json["client_id"],
-    adresseKin: json["adresse_kin"],
-    montant: double.parse(json["montant"]),
-    taux: json["taux"],
-    cout: double.parse(json["cout"]),
-    statutId: json["statut_id"],
-    client: Client.fromJson(json["client"]),
+    synopsis: json["synopsis"],
+    note: json["note"],
+    dateDeSortie: json["date_de_sortie"],
   );
 
   Map<String, dynamic> toJson() => {
     "id": id,
     "created_at": createdAt!.toIso8601String(),
     "updated_at": updatedAt!.toIso8601String(),
-    "user_id": userId,
-    "client_id": clientId,
-    "adresse_kin": adresseKin,
-    "montant": montant,
-    "taux": taux,
-    "cout": cout,
-    "statut_id": statutId,
-    "client": client!.toJson()
+    "synopsis": synopsis,
+    "note": note,
+    "dateDeSortie": dateDeSortie,
   };
 }
